8e2c8fb36af522e888b53d15ecd20847f63d2291
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2022-11-24 11:32:54 +0000

    Add Position Model class

--- app/src/main/java/ldts/terrarialike/model/Position.java
+++ app/src/main/java/ldts/terrarialike/model/Position.java
@@ -1,5 +1,59 @@
 package ldts.terrarialike.model;
 
+import ldts.terrarialike.exceptions.InvalidPositionException;
+
+
 public class Position {
-    
+    static int Y_MIN = 0;
+    static int Y_MAX = 200;
+
+
+    private int x;
+
+
+    private int y;
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public Position(int x, int y) throws InvalidPositionException{
+        if(y < Y_MIN ){
+            throw new InvalidPositionException("y is below Y_MIN");
+        }
+        if(y > Y_MAX){
+            throw new InvalidPositionException("y if above Y_MAX");
+        }
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + y;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Position other = (Position) obj;
+        if (x != other.x)
+            return false;
+        if (y != other.y)
+            return false;
+        return true;
+    }
 }
