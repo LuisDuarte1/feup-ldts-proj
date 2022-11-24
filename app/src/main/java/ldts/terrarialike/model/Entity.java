99a020e478b44b4023493fb3e44e148394c490d3
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2022-11-24 13:06:18 +0000

    Add Entity class

--- app/src/main/java/ldts/terrarialike/model/Entity.java
+++ app/src/main/java/ldts/terrarialike/model/Entity.java
@@ -1,5 +1,29 @@
 package ldts.terrarialike.model;
 
 public abstract class Entity {
+    private Position position;
+    private int hp;
+    
+    public Entity(Position position, int hp) {
+        this.position = position;
+        this.hp = hp;
+    }
+    
+    public Position getPosition() {
+        return position;
+    }
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+    public int getHp() {
+        return hp;
+    }
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public boolean collidesWith(Entity t){
+        return t.getPosition() == position;
+    }
     
 }
