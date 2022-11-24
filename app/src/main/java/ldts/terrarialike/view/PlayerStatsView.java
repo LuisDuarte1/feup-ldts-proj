658506a987f3bdee6f8bd9d0c2889dd2979196ed
TomasdosBeefs <tomaspr123@gmail.com>
2022-11-24 23:11:24 +0000

    PlayerStatsView view

--- app/src/main/java/ldts/terrarialike/view/PlayerStatsView.java
+++ app/src/main/java/ldts/terrarialike/view/PlayerStatsView.java
@@ -1,5 +1,38 @@
 package ldts.terrarialike.view;
 
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import ldts.terrarialike.exceptions.InvalidPositionException;
+import ldts.terrarialike.model.Inventory;
+import ldts.terrarialike.model.Player;
+import ldts.terrarialike.model.Position;
+
 public class PlayerStatsView implements ElementView{
+
+    private Player player;
+
+    public PlayerStatsView(Player player) {
+        this.player = player;
+    }
+    @Override
+    public void draw(TextGraphics graphics) {
+        try {
+            graphics.setBackgroundColor(TextColor.Factory.fromString("#000000"));
+            graphics.setForegroundColor(TextColor.Factory.fromString("#FFFFFF"));
+            graphics.putString(0, 0, "Player Stats");
+            graphics.drawLine(new TerminalPosition(0, 1), new TerminalPosition(5, 1), '─');
+            graphics.putString(0, 2, "Hp: " + "100");
+            graphics.putString(0,4, "Inventory: " + "30");
+        } catch ( Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+
     
 }
