e12c4564f2a8c678ba62764e8290a00fe2eb9c65
TomasdosBeefs <tomaspr123@gmail.com>
2022-12-09 11:27:51 +0000

    GameEvent

--- app/src/main/java/ldts/terrarialike/controller/GameEvent.java
+++ app/src/main/java/ldts/terrarialike/controller/GameEvent.java
@@ -1,5 +1,11 @@
 package ldts.terrarialike.controller;
 
+import ldts.terrarialike.model.World;
+
+import java.util.List;
+
 public interface GameEvent {
+
+    List<GameEvent> execute(World world);
     
 }
--- app/src/main/java/ldts/terrarialike/exceptions/GameEventsIsNullException.java
+++ app/src/main/java/ldts/terrarialike/exceptions/GameEventsIsNullException.java
@@ -1,2 +1,8 @@
-package ldts.terrarialike.exceptions;public class GameEventsIsEmptyException {
-}
+package ldts.terrarialike.exceptions;
+
+public class GameEventsIsNullException extends Throwable {
+    public GameEventsIsNullException(String error_message) {
+        super(error_message);
+    }
+    }
+
