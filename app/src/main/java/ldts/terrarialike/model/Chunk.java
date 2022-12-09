b9ad1fcaa9cec216fa4239bf79388da18f3e7476
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2022-12-09 10:37:57 +0000

    Move some methods to world to make getting the blocks easier

--- app/src/main/java/ldts/terrarialike/model/Chunk.java
+++ app/src/main/java/ldts/terrarialike/model/Chunk.java
@@ -81,6 +81,30 @@ public class Chunk {
                 return false;
             }
         }
+
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + position;
+            return result;
+        }
+
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Chunk other = (Chunk) obj;
+            if (position != other.position)
+                return false;
+            return true;
+        }
     }
 
 
