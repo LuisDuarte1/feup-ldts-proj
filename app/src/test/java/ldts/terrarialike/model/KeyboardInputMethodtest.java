ff06a32e0a1d5abda3b94b0901ba1ed69f695c33
TomasdosBeefs <tomaspr123@gmail.com>
2022-12-09 11:36:44 +0000

    KeyboardInputMethodtest and Chunktest problem fixing

--- app/src/test/java/ldts/terrarialike/model/KeyboardInputMethodtest.java
+++ app/src/test/java/ldts/terrarialike/model/KeyboardInputMethodtest.java
@@ -5,32 +5,9 @@ import org.junit.jupiter.api.Test;
 
 public class KeyboardInputMethodtest {
 
-    @Test
-    public void testGetPosx(){
-        KeyboardInputMethod keyboardInputMethodtest = new KeyboardInputMethod(3,4);
-        Assertions.assertEquals(3,keyboardInputMethodtest.getPosx());
-    }
 
-    @Test
-    public void testGetPosy(){
-        KeyboardInputMethod keyboardInputMethod = new KeyboardInputMethod(3,4);
-        Assertions.assertEquals(4,keyboardInputMethod.getPosy());
 
-    }
 
-    @Test
-    public void testsetPosx(){
-        KeyboardInputMethod keyboardInputMethod = new KeyboardInputMethod(3,4);
-        keyboardInputMethod.setPosx(6);
-        Assertions.assertEquals(6,keyboardInputMethod.getPosx());
-    }
-
-    @Test
-    public void testsetPosy(){
-        KeyboardInputMethod keyboardInputMethod = new KeyboardInputMethod(3,4);
-        keyboardInputMethod.setPosy(6);
-        Assertions.assertEquals(6,keyboardInputMethod.getPosy());
-    }
 
     @Test
     public void testgetkey(){
