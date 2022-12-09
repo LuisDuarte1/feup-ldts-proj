0a90892e9a576b64fabb84c8e4e27591990ea29c
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2022-12-09 11:36:11 +0000

    Add place block item interaction

--- app/src/main/java/ldts/terrarialike/controller/itemInteractions/UseBlockItemInteraction.java
+++ app/src/main/java/ldts/terrarialike/controller/itemInteractions/UseBlockItemInteraction.java
@@ -2,7 +2,10 @@ package ldts.terrarialike.controller.itemInteractions;
 
 
 import ldts.terrarialike.controller.ItemInteraction;
+import ldts.terrarialike.exceptions.InvalidPositionException;
+import ldts.terrarialike.model.Block;
 import ldts.terrarialike.model.BlockInfo;
+import ldts.terrarialike.model.Chunk;
 import ldts.terrarialike.model.InteractionType;
 import ldts.terrarialike.model.Item;
 import ldts.terrarialike.model.Position;
@@ -25,7 +28,22 @@ public class UseBlockItemInteraction implements ItemInteraction{
     @Override
     public void execute(World one, InteractionType interactionType, Item item) {
         if(interactionType == InteractionType.USE && one.getBlock(desiredPosition) == null){
-            
+            Integer chunkID = desiredPosition.getX() % Chunk.CHUNK_SIZE;
+            Chunk desiredChunk = null;
+            for (Chunk chunk : one.getChunks()) {
+                if(chunk.getChunkID() == chunkID){
+                    desiredChunk = chunk;
+                    break;
+                }
+            }
+            if(desiredChunk == null) return;
+            try {
+                desiredChunk.addBlock(new Block(desiredPosition, blockInfo));
+            } catch (InvalidPositionException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return;
+            }
         }
     }
     
