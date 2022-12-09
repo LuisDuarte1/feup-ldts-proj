0e446676cc2274fb5ae09a61efac9297f50e7688
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2022-12-09 16:47:50 +0000

    Abstract gravity to all entities

--- app/src/main/java/ldts/terrarialike/controller/PlayerController.java
+++ app/src/main/java/ldts/terrarialike/controller/PlayerController.java
@@ -6,57 +6,19 @@ import ldts.terrarialike.model.Player;
 import ldts.terrarialike.model.Position;
 import ldts.terrarialike.model.World;
 
-public class PlayerController {
-
-    //FIXME (luisd): not a good way to do this, maybe we need to refactor entities to account for physics
-    
-    private Player player;
+public class PlayerController extends EntityController {
 
 
     private Integer startFallingHeight;
-    
+
     
     
     
     public PlayerController(Player player) {
-        this.player = player;
+        super(player);
         this.startFallingHeight = -1;
     }
     
-    private Integer calculateGravityDamage(Integer fallenHeight){
-        if(fallenHeight <= 3) return 0;
-        return fallenHeight*4;
-
-    }
-
-    private void damagePlayerDueGravity(){
-        player.setHp(player.getHp() - calculateGravityDamage(startFallingHeight));
-        startFallingHeight = -1;
-    }
-    
-
-    private void applyGravity(World world){
-        Position blockBelowPosition = null;
-        try {
-            blockBelowPosition = new Position(player.getPosition().getX(), player.getPosition().getY()-1);
-        } catch (InvalidPositionException e) {
-            //if this ever happens, we assume that it isn't valling and we damage the player if
-            //necessary
-            if(startFallingHeight > 0){
-                damagePlayerDueGravity();
-                
-            }
-            return;
-        }
-
-        Block blockBelow = world.getBlock(blockBelowPosition);
-        if(blockBelow == null){
-                player.setPosition(blockBelowPosition);
-                startFallingHeight += 1;
-        } else{
-            damagePlayerDueGravity();
-        }
-    }
 
     public void tick(World world){
         applyGravity(world);
