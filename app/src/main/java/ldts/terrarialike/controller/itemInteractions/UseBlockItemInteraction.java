5b441f33517155b13b97535d24ec5498e9690468
TomasdosBeefs <tomaspr123@gmail.com>
2022-12-09 12:05:29 +0000

    Change UseBlockItemInteraction to comply with new ItemInteraction

--- app/src/main/java/ldts/terrarialike/controller/itemInteractions/UseBlockItemInteraction.java
+++ app/src/main/java/ldts/terrarialike/controller/itemInteractions/UseBlockItemInteraction.java
@@ -1,6 +1,7 @@
 package ldts.terrarialike.controller.itemInteractions;
 
 
+import ldts.terrarialike.controller.GameEvent;
 import ldts.terrarialike.controller.ItemInteraction;
 import ldts.terrarialike.exceptions.InvalidPositionException;
 import ldts.terrarialike.model.Block;
@@ -10,6 +11,8 @@ import ldts.terrarialike.model.InteractionType;
 import ldts.terrarialike.model.Item;
 import ldts.terrarialike.model.Position;
 import ldts.terrarialike.model.World;
+
+import java.util.List;
 
 public class UseBlockItemInteraction implements ItemInteraction{
 
@@ -25,8 +28,7 @@ public class UseBlockItemInteraction implements ItemInteraction{
 
 
 
-    @Override
-    public void execute(World one, InteractionType interactionType, Item item) {
+    public List<GameEvent> execute(World one, InteractionType interactionType, Item item) {
         if(interactionType == InteractionType.USE && one.getBlock(desiredPosition) == null){
             Integer chunkID = desiredPosition.getX() % Chunk.CHUNK_SIZE;
             Chunk desiredChunk = null;
@@ -36,15 +38,16 @@ public class UseBlockItemInteraction implements ItemInteraction{
                     break;
                 }
             }
-            if(desiredChunk == null) return;
+            if(desiredChunk == null) return null;
             try {
                 desiredChunk.addBlock(new Block(desiredPosition, blockInfo));
             } catch (InvalidPositionException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
-                return;
+                return null;
             }
         }
+        return null;
     }
     
 }
