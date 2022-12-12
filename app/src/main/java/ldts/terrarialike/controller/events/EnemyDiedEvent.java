6c579ea09cf3acb66022c15e399310cf6b2e2cdc
António Romão <antonio.romao03@gmail.com>
2022-12-12 23:06:52 +0000

    Implemented EnemyDiedEvent

--- app/src/main/java/ldts/terrarialike/controller/events/EnemyDiedEvent.java
+++ app/src/main/java/ldts/terrarialike/controller/events/EnemyDiedEvent.java
@@ -1,15 +1,22 @@
 package ldts.terrarialike.controller.events;
 
 import ldts.terrarialike.controller.GameEvent;
+import ldts.terrarialike.model.Enemy;
 import ldts.terrarialike.model.World;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EnemyDiedEvent implements GameEvent{
-
-
+    private Enemy enemy;
+    public EnemyDiedEvent(Enemy enemy) {
+        this.enemy = enemy;
+    }
     @Override
     public List<GameEvent> execute(World world) {
-        return null;
+        List<GameEvent> list = new ArrayList<>();
+        world.removeEnemy(enemy);
+        return list;
     }
+
 }
