57a8a1cd880d8b505584464c9f1a93453b83deaa
TomasdosBeefs <tomaspr123@gmail.com>
2022-12-23 16:37:39 +0000

    DefaultAttackItem and Test

--- app/src/test/java/ldts/terrarialike/controller/ItemInteraction/direction/DefaultAttackItemTest.java
+++ app/src/test/java/ldts/terrarialike/controller/ItemInteraction/direction/DefaultAttackItemTest.java
@@ -1,11 +1,13 @@
 package ldts.terrarialike.controller.ItemInteraction.direction;
 
+import ldts.terrarialike.controller.GameEvent;
+import ldts.terrarialike.controller.itemInteractions.DirectionItemInteraction;
+import ldts.terrarialike.controller.itemInteractions.direction.DefaultAttackItem;
 import ldts.terrarialike.exceptions.InvalidPositionException;
 import ldts.terrarialike.exceptions.InvalidSizeException;
-import ldts.terrarialike.model.Enemy;
-import ldts.terrarialike.model.Player;
-import ldts.terrarialike.model.Position;
-import ldts.terrarialike.model.World;
+import ldts.terrarialike.model.*;
+import ldts.terrarialike.utils.WorldUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -21,7 +23,8 @@ public class DefaultAttackItemTest {
 
     public void findNearEnemiesTest() throws InvalidSizeException, InvalidPositionException {
 
-        World world = new World();
+        World world = Mockito.mock(World.class);
+        WorldUtils worldUtils = Mockito.mock(WorldUtils.class);
 
         List<Enemy> enemies = new ArrayList<>();
 
@@ -34,34 +37,63 @@ public class DefaultAttackItemTest {
         Position position1 = Mockito.mock(Position.class);
         Position position2 = Mockito.mock(Position.class);
 
-        Mockito.when(position1.getX()).thenReturn(1);
-        Mockito.when(position1.getY()).thenReturn(1);
-        Mockito.when(position2.getX()).thenReturn(2);
-        Mockito.when(position2.getY()).thenReturn(2);
+        Mockito.when(position1.getX()).thenReturn(2);
+        Mockito.when(position1.getY()).thenReturn(2);
+        Mockito.when(position2.getX()).thenReturn(3);
+        Mockito.when(position2.getY()).thenReturn(3);
 
 
         Player player = Mockito.mock(Player.class);
+        PlayerLogs playerLogs = Mockito.mock(PlayerLogs.class);
+        Mockito.when(player.getPlayerLogs()).thenReturn(playerLogs);
 
-        Mockito.when(position.getX()).thenReturn(1);
-        Mockito.when(position.getY()).thenReturn(1);
+        Mockito.when(position.getX()).thenReturn(0);
+        Mockito.when(position.getY()).thenReturn(0);
         Mockito.when(world.getPlayer()).thenReturn(player);
         Mockito.when(world.getPlayer().getPosition()).thenReturn(position);
 
+        Item item = Mockito.mock(Item.class);
+        Inventory inventory = Mockito.mock(Inventory.class);
+        Mockito.when(player.getInventory()).thenReturn(inventory);
+        Mockito.when(player.getInventory().getSelectedItem()).thenReturn(item);
+
+        Mockito.when(enemy1.getPosition()).thenReturn(position1);
+        Mockito.when(enemy2.getPosition()).thenReturn(position2);
+        Mockito.when(world.getEnemiesList()).thenReturn(enemies);
 
 
+        DefaultAttackItem defaultAttackItem = new DefaultAttackItem();
+
+        List<GameEvent> gameEvents = defaultAttackItem.execute(world, InteractionType.ATTACK, item, worldUtils);
+
+        Assertions.assertEquals(new ArrayList<>(), gameEvents);
 
 
+        defaultAttackItem.setDirection(true);
+
+        gameEvents = defaultAttackItem.execute(world, InteractionType.ATTACK, item, worldUtils);
+
+        Mockito.verify(enemy1, Mockito.times(1)).setHp(Mockito.anyInt());
+        Mockito.verify(enemy2, Mockito.times(0)).setHp(Mockito.anyInt());
 
 
+        enemies = new ArrayList<>();
+
+        Enemy enemy3 = Mockito.mock(Enemy.class);
+        Enemy enemy4 = Mockito.mock(Enemy.class);
+        enemies.add(enemy3);
+        enemies.add(enemy4);
 
 
-    }
+        Mockito.when(enemy3.getPosition()).thenReturn(position1);
+        Mockito.when(enemy4.getPosition()).thenReturn(position2);
 
-    @Test
+        defaultAttackItem.setDirection(false);
 
-    public void testExecute() {
+        gameEvents = defaultAttackItem.execute(world, InteractionType.ATTACK, item, worldUtils);
 
-
+        Mockito.verify(enemy3, Mockito.times(0)).setHp(Mockito.anyInt());
+        Mockito.verify(enemy4, Mockito.times(0)).setHp(Mockito.anyInt());
 
     }
 }
