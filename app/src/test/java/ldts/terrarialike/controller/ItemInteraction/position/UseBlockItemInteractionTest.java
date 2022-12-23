5de3a7124da6683f3385f5d74e17deb47d976d89
TomasdosBeefs <tomaspr123@gmail.com>
2022-12-23 16:42:39 +0000

    UseBlockItemInteractoinTest

--- app/src/test/java/ldts/terrarialike/controller/ItemInteraction/position/UseBlockItemInteractionTest.java
+++ app/src/test/java/ldts/terrarialike/controller/ItemInteraction/position/UseBlockItemInteractionTest.java
@@ -1,35 +1,80 @@
 package ldts.terrarialike.controller.ItemInteraction.position;
 
+import ldts.terrarialike.controller.GameEvent;
 import ldts.terrarialike.controller.itemInteractions.position.UseBlockItemInteraction;
 import ldts.terrarialike.exceptions.InvalidPositionException;
+import ldts.terrarialike.exceptions.InvalidQuantityException;
 import ldts.terrarialike.exceptions.InvalidSizeException;
-import ldts.terrarialike.model.BlockInfo;
-import ldts.terrarialike.model.InteractionType;
-import ldts.terrarialike.model.Item;
-import ldts.terrarialike.model.World;
+import ldts.terrarialike.exceptions.ItemNotFoundException;
+import ldts.terrarialike.model.*;
+import ldts.terrarialike.utils.WorldUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class UseBlockItemInteractionTest {
 
     @Test
 
-    public void testExecute() throws InvalidSizeException, InvalidPositionException {
+    public void testExecute() throws InvalidSizeException, InvalidPositionException, InvalidQuantityException, ItemNotFoundException {
+
+        BlockInfo blockInfo = Mockito.mock(BlockInfo.class);
 
         Item item = Mockito.mock(Item.class);
-        BlockInfo blockInfo = Mockito.mock(BlockInfo.class);
-        World world = new World();
+
+
+
+        WorldUtils worldUtils = Mockito.mock(WorldUtils.class);
+        World world = Mockito.mock(World.class);
+
+        Position position = Mockito.mock(Position.class);
+
+        Mockito.when(position.getX()).thenReturn(0);
+        Mockito.when(position.getY()).thenReturn(0);
+
+        Inventory inventory = Mockito.mock(Inventory.class);
+        Player player = Mockito.mock(Player.class);
+        Mockito.when(world.getPlayer()).thenReturn(player);
+        Mockito.when(player.getInventory()).thenReturn(inventory);
+
+
+        PlayerLogs playerLogs = Mockito.mock(PlayerLogs.class);
+        Mockito.when(player.getPlayerLogs()).thenReturn(playerLogs);
+
+        List<Chunk> chunks = new ArrayList<>();
+        Chunk chunk = Mockito.mock(Chunk.class);
+
+        chunks.add(chunk);
+
+        Mockito.when(chunk.getChunkID()).thenReturn(0);
+
+        Mockito.when(world.getChunks()).thenReturn(chunks);
 
         UseBlockItemInteraction useBlockItemInteraction = new UseBlockItemInteraction(blockInfo);
 
-        Assertions.assertEquals(new ArrayList<>(),useBlockItemInteraction.execute(world, InteractionType.ATTACK, item));
-        Assertions.assertEquals(new ArrayList<>(),useBlockItemInteraction.execute(world, InteractionType.DEFENSE, item));
-        Assertions.assertEquals(new ArrayList<>(),useBlockItemInteraction.execute(world, InteractionType.DESTROY, item));
+        useBlockItemInteraction.setDesiredPosition(position);
+
+        List<GameEvent> gameEvents= useBlockItemInteraction.execute(world,InteractionType.USE,item, worldUtils);
+
+        Mockito.verify(chunk, Mockito.times(1)).addBlock(Mockito.any());
+        Mockito.verify(inventory, Mockito.times(1)).remove(item,1);
+
+
+        // chunkID = 0
+
+
+
+
+
+
+
+
 
     }
+
 
 
 }
